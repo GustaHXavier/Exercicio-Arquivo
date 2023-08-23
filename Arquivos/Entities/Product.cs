5f564d7718c827e68a4f3ff295a53c90003d7dea
Gustavo Henrique <gugu2305@hotmail.com>
2023-08-23 17:17:46 -0300

    Exercicios de Arquivos

---
+++ Arquivos/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Entities {
+    internal class Product {
+
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public double Preco { get; set; }
+
+        public Product(string nome, int quantidade, double preco) {
+            Nome = nome;
+            Quantidade = quantidade;
+            Preco = preco;
+        }
+
+        public double Total() {
+            return Quantidade * Preco;
+        }
+    }
+}
---
+++ Arquivos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using Entities;
+using System.Globalization;
+
+namespace arquivos {
+    class Program {
+        static void Main(string[] args) {
+
+            string sourceFile = @"C:\Users\gugu2\OneDrive\Área de Trabalho\temp\origem.txt";
+
+            try {
+                string[] linhas = File.ReadAllLines(sourceFile);
+
+                string diretorioDoArquivo = Path.GetDirectoryName(sourceFile);
+                string diretorioFinal = diretorioDoArquivo + @"\out";
+                string ArquivoDestino = diretorioFinal + @"\summary.txt";
+
+                Directory.CreateDirectory(diretorioFinal);
+
+
+                using (StreamWriter sw = File.AppendText(ArquivoDestino)) {
+
+                    foreach (string linha in linhas) {
+
+                        string[] produtos = linha.Split(',');
+
+                        string nome = produtos[0];
+                        double preco = double.Parse(produtos[1], CultureInfo.InvariantCulture);
+                        int quantidade = int.Parse(produtos[2]);
+
+
+                        Product produto = new Product(nome, quantidade, preco);
+
+                        sw.Write(produto.Nome + ", " + produto.Total().ToString("F2", CultureInfo.InvariantCulture) + "\n");
+
+                    }
+                }
+            }
+            catch(IOException e) {
+                Console.WriteLine("ERRO");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
+
+
